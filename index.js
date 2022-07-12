f2f2e7c41bda87a7376110d675b2cec08e7d2139
debz1 <debenshakya1@gmail.com>
2022-07-12 09:11:44 +0545

    day 2 multiplier

---
+++ index.js
@@ -0,0 +1,19 @@
+let mul =7;
+
+for (let i=1;i<10;i++){
+    // console.log(i*mul)
+    result=i * mul
+    console.log(`${i} *  ${mul} = ${result}`)
+}
+let j=0
+while(j<10){
+    result=j * mul
+    console.log(`${j} *  ${mul} = ${result}`)
+    j++;
+}
+let k=1
+do{
+    result=k * mul
+    console.log(`${k} *  ${mul} = ${result}`)
+    k++;
+}while(k <= 10)
