964ea5cfff01837649747681db879720902f081b
debz1 <debenshakya1@gmail.com>
2022-07-19 09:03:47 +0545

    day 5

--- index.js
+++ index.js
@@ -13,3 +13,40 @@ const Times=()=>{
 
 }
 const inter=setInterval(Times,1000)
+
+
+
+//next day
+// let arr=[10,20,30]
+
+// let a=arr[0]
+// let b=arr[1]
+// let c=arr[2]
+// console.log(a)
+
+
+// let a={
+//     movies:{
+//         "name":"kgf",
+//         "price":1200,
+//         "content":"flim",
+//     },
+//     movies1:{
+//         "name":"kgf2",
+//         "price":12000,
+//         "content":"flim",
+//     }
+   
+// }
+// const {movies}=a
+// const {name,price}=movies
+// // console.log(name)
+
+// const arr=({name,price,...every})=>{
+//     console.log(name)
+//     console.log(price)
+//     console.log(every)
+//     const {content}=every
+//     console.log(content)
+// }
+// arr(movies)
