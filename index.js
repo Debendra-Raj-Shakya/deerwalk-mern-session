dba2049f2fc0efd47fd2f79920a7e21275c697d8
debz1 <debenshakya1@gmail.com>
2022-07-26 10:13:44 +0545

    day  10

--- index.js
+++ index.js
@@ -1,30 +1,14 @@
-// const EventEmitter=require('events')
-// const emitter= new EventEmitter();
-// emitter.on('hello',()=>{
-//     console.log('hello')
 
-// })
-// emitter.emit('hello')
 
-const fs = require("fs");
-const path = require("path");
-console.log("text");
-const readfile = fs.readdirSync("./node-app");
-readfile.map((a) => {
-  let ext = path.extname(a);
-  if (ext === ".txt") {
-    if (!fs.existsSync("text-folder")) {
-      fs.mkdirSync("text-folder");
-    }
-    fs.renameSync(a, "./text-folder/" + a);
-  }
+const data={
+  name:"debendra",
+  age:25,
+  
+}
+let jsonData=JSON.stringify(data)
 
-  if (ext === ".js") {
-    if (!fs.existsSync("js-folder")) {
-      fs.mkdirSync("js-folder");
-    }
-    fs.renameSync(a, "./js-folder/" + a);
-  }
-  console.log(ext);
-});
-console.log(readfile);
+console.log(jsonData)
+console.log(typeof jsonData)
+let objectData=JSON.parse(jsonData)
+console.log(  objectData)
+console.log( typeof objectData)
--- node-server/node-server.js
+++ node-server/node-server.js
@@ -1,30 +1,55 @@
 const { resolveSoa } = require("dns");
 const http = require("http");
-
+const fs =require('fs')
+const quotesData=require('../quotes.json');
+// console.log(quotesData)
 // http.createServer(function (req, res) {
 //     res.write('Hello World!'); //write a response to the client
 //     res.end(); //end the response
 //   }).listen(8080)
 
-const server = http.createServer((req, res) => {
-//   console.log("server running");
-//   res.writeHead(200)
-//   res.write('res written')
-//   res.end()
-  console.log(req.url)
-  if(req.url==="/test"){
-    res.write('test')
-    res.end()
-  }else if(req.url==="/"){
-    res.write  ('index')
-    res.end("page")
+const generateRandomNumber=()=>{
+  const a =Math.floor( Math.random() *(quotesData.quotes.length -1) +1)
+  console.log(a)
+  return a
+}
 
+generateRandomNumber()
+const server = http.createServer((req, res) => {
+  if(req.url==="/quotes"){
+    res.writeHead(200,{"Content-Type": "application/json"})
+    const randomQuote=quotesData.quotes[generateRandomNumber()]
+    console.log(randomQuote)
+    res.end(JSON.stringify(randomQuote))
   }else{
     res.writeHead(404)
-    res.write('page not found')
-    res.end()
+    res.end("pagenot found")
   }
-});
+  // console.log("server running");
+  // res.writeHead(200,{"Content-Type": "application/json"})
+  // if(req.url==="/quotes"){
+  //   const readfile=fs.readFileSync('../quotes.json')
+  //   const objquotes=JSON.stringify(readfile)
+  //   const arrayobj=[...objquotes]
+  //   console.log(arrayobj)
+  //   res.end()
+  }
+
+  // console.log(req.url)
+  // if(req.url==="/test"){
+  //   res.write('test')
+  //   res.end()
+  // }else if(req.url==="/"){
+  //   res.write  ('index')
+  //   res.end("page")
+
+  // }else{
+  //   res.writeHead(404)
+  //   res.write('page not found')
+  //   res.end()
+  // }
+// }
+);
 server.listen(8080, () => {
   console.log("server started at 8080");
 
