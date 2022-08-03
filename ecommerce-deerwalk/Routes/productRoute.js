b09f45a1f286bc1762f69f06efc723b0972ab5c4
debz1 <debenshakya1@gmail.com>
2022-08-03 08:48:51 +0545

    day 12

--- ecommerce-deerwalk/Routes/productRoute.js
+++ ecommerce-deerwalk/Routes/productRoute.js
@@ -1,11 +1,39 @@
-const productData=require('../data/productData.json')
+const productData = require("../data/productData.json");
 
-const express=require('express')
-const router=express.Router()
+const express = require("express");
+const { query } = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+//    const query=req.query
+//     console.log(typeof query)
+if(req.query.category &&req.query.price){
+    let queryfilter=productData.filter((product)=>product.category==req.query.category && req.query.price==product.price ) 
+    res.json(queryfilter)
+    return
+}
+if(req.query.category ){
+    let queryfilter=productData.filter((product)=>product.category==req.query.category  ) 
+    console.log(queryfilter)
+  res.json(queryfilter);
+  return;
+
+} if( req.query.price){
+    let queryfilter=productData.filter((product)=>req.query.price==product.price ) 
+    res.json(queryfilter)
+    return;
+
+}
+
+  res.json(queryfilter);
 
 
-router.get('/api/products',(req,res)=>res.json(productData))
+});
 
-router.get('/api/products/1',(req,res)=>res.json(productData[0]))
+// router.get("/:id", (req, res) => {
+//   console.log(req.params);
+//   const pid = req.params.id;
+//   res.json(productData[pid]);
+// });
 
-module.exports=router
+module.exports = router;
--- ecommerce-deerwalk/server.js
+++ ecommerce-deerwalk/server.js
@@ -10,8 +10,8 @@ const server=express();//cannot create more than once
 
 
 server.get("/",(req,res)=>res.send("index file"))
+server.use("/api/products",ProductRouter)//routeused
 server.get('*',(req,res)=>res.send('pagenot found'))
-server.use(ProductRouter)//routeused
 
 
 
