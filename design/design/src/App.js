50ce16bb6d99171890688ee68016bccc54a8a80d
Debendra-Shakya <110882283+Debendra-Shakya@users.noreply.github.com>
2022-08-26 09:19:54 +0545

    day 18

---
+++ design/design/src/App.js
@@ -0,0 +1,35 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <>
+      <div className="topnav">
+        <a className="active" href="#home">
+          Home
+        </a>
+        <div className="navright">
+          <a href="#news">News</a>
+          <a href="#contact">Contact</a>
+          <a href="#about">blog</a>
+          <a href="#about">speakers</a>
+          <a href="#about">pages</a>
+          <a href="#about">contact</a>
+          <a href="#about">let make it</a>
+        </div>
+      </div>
+      <div className="imgbg">
+        <img src="./a.png" alt=""/>
+        <h1>hello world</h1>
+        <div className="todo">
+        <Todo/>
+
+        </div>
+      </div>
+
+    </>
+  );
+}
+
+export default App;
---
+++ design/design/src/components/Todo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Todo = () => {
+  return (
+    <div>
+      <form>
+        <input type='text' placeholder="enter text" />
+        <button type='submit'>send</button>
+      </form>
+    </div>
+  )
+}
+
+export default Todo
